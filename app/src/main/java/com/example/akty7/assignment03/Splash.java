6639a8272f563e4bc46c8680acc71d330f217edc
Akshit Tyagi <akty.tyagi7@gmail.com>
2016-01-16 17:11:01 +0530

    Splash Activity added

---
+++ app/src/main/java/com/example/akty7/assignment03/Splash.java
@@ -0,0 +1,26 @@
+package com.example.akty7.assignment03;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAYTIME=2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent=new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAYTIME);
+    }
+}
