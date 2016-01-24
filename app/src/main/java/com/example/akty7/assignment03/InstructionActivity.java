c58fbf0ea1692c7d11bf7cd001dda0820bcf2104
karandwivedi42 <karan.dwivedi42@gmail.com>
2016-01-24 21:01:51 +0530

    Layout Changes

--- app/src/main/java/com/example/akty7/assignment03/InstructionActivity.java
+++ app/src/main/java/com/example/akty7/assignment03/InstructionActivity.java
@@ -6,9 +6,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class InstructionActivity extends AppCompatActivity {
-
+    TextView txtView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,16 @@ public class InstructionActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         //TODO:Add instructions for this activity
+        String str="Instructions: \n " +
+                "This is a user registration app for students of COP290. " +
+                "The students are required to put in their valid Entry Numbers along with their names." +
+                "If there is any invalid entry, an error message specific to the invalid entry is shown. "+
+                "Please avoid names greater than character length 16, as this the maximum size that the server will accept for user registration. "+
+                "The names shouldn't contain numbers or special characters as that will be considered an invalid name.";
+        txtView=(TextView) findViewById(R.id.textView);
+        txtView.setText(str);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 
